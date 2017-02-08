f02b02c4607accf99231c2c71a7fbece5e6293b6
ztokuno <z.tokuno@gmail.com>
2017-02-08 15:15:39 -0800

    Add files via upload

---
+++ PfamClient.py
@@ -0,0 +1,43 @@
+# Zachary Tokuno (ztokuno@ucsc.edu)
+
+import sys
+from prody import *
+
+class PfamClient:
+    '''Handle pfam IO by searching for conserved domains with PDB ID or query sequence.'''
+    def __init__(self):
+        return
+
+    def doPfamSearch(self, pdb, query):
+        '''Search pfam using PDB ID or query sequence. Must give pdb and query sequence.'''
+        try:
+            pfamResults = searchPfam(pdb)
+        except IndexError:
+            sys.stderr.write('*****************************************************\n')
+            sys.stderr.write('PDB ID look up failed. Searching with query sequence.\n')
+            sys.stderr.write('*****************************************************\n')
+            pfamResults = searchPfam(query)
+        return pfamResults
+
+    def getConservedDomains(self, pfamResults):
+        '''Parse out conserved domains found in the PDB or query sequence (homolog).'''
+        # python3 returns a dict_keys object, so cast into a list
+        accession = list(pfamResults.keys())[0]
+        conservedDomains = []
+        for alignment in pfamResults[accession]['locations']:
+            start = alignment['ali_start']
+            end = alignment['ali_end']
+            conservedDomains.append((start, end))
+        return conservedDomains
+
+
+# pfamer = PfamClient()
+# pfamResults = pfamer.doPfamSearch('4xmm', 'MKRESHKHAEQARRNRLAVALHELASLIPAEWKQQNVSAAPSKATTVEAACRYIRHLQQNGST')
+# conservedDomains = pfamer.getConservedDomains(pfamResults)
+
+def main():
+    # Something important to put here
+    return
+
+if __name__ == '__main__':
+    main()
---
+++ biomongoose.py
@@ -0,0 +1,26 @@
+# Zachary Tokuno (ztokuno@ucsc.edu)
+
+from BlastClient import *
+from BlastParser import *
+from PfamClient import *
+
+def main():
+    print('BLAST...')
+    blaster = BlastClient()
+    blastResults = blaster.doBlast()
+    hsps = blaster.getHsps(blastResults)
+    query = blaster.getQuery()
+
+    print('Parsing...')
+    parser = BlastParser()
+    parser.getHspList(hsps)
+    pdb = parser.getPdb()
+
+    print('Pfam...')
+    pfamer = PfamClient()
+    pfamResults = pfamer.doPfamSearch(pdb, query)
+    conservedDomains = pfamer.getConservedDomains(pfamResults)
+    print(conservedDomains)
+
+if __name__ == '__main__':
+    main()
